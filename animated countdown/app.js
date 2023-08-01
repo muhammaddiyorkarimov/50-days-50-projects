e9ffa9c87db8dc9226e02d721fd6d846cf19d3bc
Muhammaddiyor <muhammaddiyor1516@gmail.com>
2023-08-01 12:27:00 +0500

    created countdown

---
+++ animated countdown/app.js
@@ -0,0 +1,39 @@
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.getElementById('replay')
+
+runAnimation()
+
+function resetDom() {
+
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
+
+  nums.forEach((num) => {
+    num.classList.value = ''
+  })
+
+  nums[0].classList.add('in')
+}
+
+function runAnimation() {
+  nums.forEach((num) => {
+    num.addEventListener('animationend', (e) => {
+      if (e.animationName === 'goIn') {
+        num.classList.remove('in')
+        num.classList.add('out')
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in')
+      } else {
+        counter.classList.add('hide')
+        finalMessage.classList.add('show')
+      }
+    })
+  })
+}
+
+replay.addEventListener('click', () => {
+  runAnimation()
+  resetDom()
+})
